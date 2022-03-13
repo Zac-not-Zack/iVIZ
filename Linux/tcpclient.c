c89dd7f630985d06cb83f156188828509a5463df
Zac Wong <78658974+Zac-not-Zack@users.noreply.github.com>
2022-03-13 10:10:41 +0100

    Add files via upload

--- Linux/tcpclient.c
+++ Linux/tcpclient.c
@@ -5,39 +5,48 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <unistd.h>
+
 int main() {
+	
 // create a socket
 int network_socket;
 network_socket = socket (AF_INET, SOCK_STREAM, 0);
+
 // specify an address for the socket
 struct sockaddr_in server_address;
 server_address.sin_family = AF_INET;
 server_address.sin_port = htons(9002);
 server_address.sin_addr.s_addr = INADDR_ANY;
 int connection_status = connect(network_socket, (struct sockaddr *) &server_address, sizeof(server_address));
+
 // check for error with the connection
 if (connection_status == -1) {
 printf("There was an error making a connection to the remote socket \n\n");
 }
-// recieve data from the server
+//printf("%d", connection_status);
+
+// receive data from the server
 char server_response[256];
 
 recv(network_socket, &server_response, sizeof(server_response), 0);
+
 // print out the server's response
 printf("The server sent the data: %s\n", server_response);
-// and then close the socket
-
+while (connection_status!= -1){
 char str1[256], str2[10];
 FILE* fp;
 
    fp = fopen ("/proc/uptime", "r");
    
    fscanf(fp, "%s %s", str1, str2);
-   printf("%s",str1);
+  // printf("%s",str1);
  char client_message[256];
  strcpy(client_message,str1);
 send(network_socket, client_message, sizeof(client_message), 0);
 
-close(network_socket);
+sleep(1);
+// and then close the socket
+//close(network_socket);
+}
 return 0;
 }
