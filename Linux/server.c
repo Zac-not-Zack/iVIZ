74d3c2f04f2214b49b4fcee03ad960183b0e2767
Zac Wong <78658974+Zac-not-Zack@users.noreply.github.com>
2022-03-26 15:06:37 +0100

    Add files via upload

---
+++ Linux/server.c
@@ -0,0 +1,106 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <sys/statvfs.h>
+
+#define PORT 30000
+#define IP_SERVER "134.59.139.192"
+
+struct typeInfo {
+char name[256];
+char OS[10];
+int sizeHD;
+int sizeHDDispo;
+int sizeRAM;
+char nameCPU[20];
+float speedCPU;
+float usageCPU;
+float temp;
+};
+
+struct statvfs stat;
+
+struct cpustat {
+    unsigned long t_user;
+    unsigned long t_nice;
+    unsigned long t_system;
+    unsigned long t_idle;
+    unsigned long t_iowait;
+    unsigned long t_irq;
+    unsigned long t_softirq;
+};
+
+int main(){
+
+	struct typeInfo info;
+	
+	int sockfd, ret;
+	struct sockaddr_in serverAddr;
+
+	int newSocket;
+	struct sockaddr_in newAddr;
+
+	socklen_t addr_size;
+
+	char buffer[1024];
+	pid_t childpid;
+
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	if(sockfd < 0){
+		printf("[-]Error in connection.\n");
+		exit(1);
+	}
+	printf("[+]Server Socket is created.\n");
+
+	memset(&serverAddr, '\0', sizeof(serverAddr));
+	serverAddr.sin_family = AF_INET;
+	serverAddr.sin_port = htons(PORT);
+	serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+
+	ret = bind(sockfd, (struct sockaddr*)&serverAddr, sizeof(serverAddr));
+	if(ret < 0){
+		printf("[-]Error in binding.\n");
+		exit(1);
+	}
+	printf("[+]Bind to port %d\n", PORT);
+
+	if(listen(sockfd, 10) == 0){
+		printf("[+]Listening....\n");
+	}else{
+		printf("[-]Error in binding.\n");
+	}
+
+
+	while(1){
+		newSocket = accept(sockfd, (struct sockaddr*)&newAddr, &addr_size);
+		if(newSocket < 0){
+			exit(1);
+		}
+		printf("Connection accepted from %s:%d\n", inet_ntoa(newAddr.sin_addr), ntohs(newAddr.sin_port));
+
+		if((childpid = fork()) == 0){
+			close(sockfd);
+
+			while(recv(newSocket, &info, sizeof(info), 0)){
+				
+					printf("From %s:%d\n", inet_ntoa(newAddr.sin_addr), ntohs(newAddr.sin_port));
+					printf("Client: %f\n", info.usageCPU);
+					//send(newSocket, buffer, strlen(buffer), 0);
+					bzero(&info, sizeof(info));
+				
+			}
+			
+		}
+
+	}
+
+	close(newSocket);
+
+
+	return 0;
+}
